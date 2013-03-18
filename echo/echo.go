9e9d448779bdd372ce2bf48842ebfa3c9c0f3259
Martin Gammelsæter <martigam@stud.ntnu.no>
2013-03-18 23:30:06 +0100

    Add -e flag to echo, similar to GNU echo.

--- echo/echo.go
+++ echo/echo.go
@@ -7,6 +7,7 @@ import (
 )
 
 var n = flag.Bool("n", false, "suppresses the newline")
+var e = flag.Bool("e", false, "enable interpretation of backslash escapes")
 
 func main() {
 	flag.Parse()
@@ -19,13 +20,45 @@ func main() {
 		}
 	}
 
-	if !*n {
-		buf.WriteString("\n")
+	str := buf.String()
+
+	if *e {
+		for i := 0; i < len(str); i++ {
+			c := str[i : i+1]
+			if c == "\\" { // Forcing one-rune string instead of rune value.
+				switch str[i+1] {
+				case 'a':
+					c = "\a"
+				case 'b':
+					c = "\b"
+				case 'c':
+					break
+				case 'e':
+					c = "\x1B"
+				case 'f':
+					c = "\f"
+				case 'n':
+					c = "\n"
+				case 'r':
+					c = "\r"
+				case 't':
+					c = "\t"
+				case 'v':
+					c = "\v"
+				//TODO Output byte with octal value
+				case '0':
+				//TODO Output byte with hex value
+				case 'x':
+				}
+				i++
+			}
+			fmt.Print(c)
+		}
+	} else {
+		fmt.Print(str)
 	}
 
-	_, err := fmt.Print(buf.String())
-
-	if err != nil {
-		panic(err)
+	if !*n {
+		fmt.Print("\n")
 	}
 }
